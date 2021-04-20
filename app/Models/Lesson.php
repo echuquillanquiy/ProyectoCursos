ea3cbd65e33b827eafdd834a6a1b2055aa53a9fb
eduardo <echuquillanquiy@gmail.com>
2021-04-20 11:33:25 -0500

    db, integracion adminlte

---
+++ app/Models/Lesson.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    //Relacion uno a uno
+
+    public function description()
+    {
+        return $this->hasOne('App\Models\Description');
+    }
+
+    //Relación uno a muchos inversa
+    public function section()
+    {
+        return $this->belongsTo('App\Models\Section');
+    }
+
+    public function platform()
+    {
+        return $this->belongsTo('App\Models\Platform');
+    }
+
+    //Relación muchos a muchos
+
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User');
+    }
+
+    //Relación uno a uno polimorfica
+
+    public function resource()
+    {
+        return $this->morphOne('App\Models\Resource', 'resourceable');
+    }
+
+    //Relación uno a muchos polimorfica
+
+    public function comments()
+    {
+        return $this->morphMany('App\Models\Comment', 'commentable');
+    }
+
+    public function reactions()
+    {
+        return $this->morphMany('App\Models\Reaction', 'reactionable');
+    }
+}
